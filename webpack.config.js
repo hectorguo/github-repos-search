4295df99798e8348900a34b350c9072b6a3fcf1a
hectorguo <hectorguo@live.com>
2017-03-31 21:05:55 -0700

    change refer path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: 'dist/',
+    publicPath: '/github-repos-search/dist/',
     filename: 'build.js'
   },
   module: {
